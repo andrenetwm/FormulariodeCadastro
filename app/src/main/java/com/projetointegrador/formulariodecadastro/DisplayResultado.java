03a6313d177022f575011d59444eb689dec52384
André Farias <andrenetwm@yahoo.com.br>
2018-07-24 00:14:22 -0300

    Initial commit

---
+++ app/src/main/java/com/projetointegrador/formulariodecadastro/DisplayResultado.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.formulariodecadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_resultado);
+    }
+}
